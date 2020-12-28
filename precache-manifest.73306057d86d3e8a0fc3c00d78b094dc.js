21d9d2bf2972d70a6d07799d03e47df70eaf39b5
Angelo Pool Rodriguez <brofan66@gmail.com>
2020-12-28 17:00:27 -0500

    Updates

---
+++ precache-manifest.73306057d86d3e8a0fc3c00d78b094dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc46105f25c087e83ced2e3dc758f994",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "0c4f663ccb6925cbd890",
+    "url": "/Markdown-Previewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/Markdown-Previewer/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-Previewer/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4f663ccb6925cbd890",
+    "url": "/Markdown-Previewer/static/js/main.74861dc9.chunk.js"
+  },
+  {
+    "revision": "c0bf455dc64f438ea03a",
+    "url": "/Markdown-Previewer/static/js/runtime-main.0442c9e5.js"
+  }
+]);
