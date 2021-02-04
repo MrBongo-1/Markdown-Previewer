75a205d311972c4a218cec92ab938bb94657e3d9
Angelo Rodriguez <brofan66@gmail.com>
2021-02-03 23:41:37 -0500

    Styled

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React from 'react';
 import './App.css';
 
 const marked = window.marked
+marked.setOptions({
+  breaks: true
+})
 class Code extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input:'# Heading\n## Sub-heading\n[Link](https://www.youtube.com/watch?v=dQw4w9WgXcQ)\n```\nfunction() {\n return true;\n}\n```\n> Blockquote\n![image](https://lh3.googleusercontent.com/W5LywwJHdo-l6Lm5Z6q2E0pziqzru05QEy5lwXDBPNAnrr5zx9oJrqefXNTGjtToW5s24pUZtQ=w640-h400-e365)\n**Bold text**\n- list item.\n`code block`'
+      input:'# Heading\n## Sub-heading\n[Link](https://www.youtube.com/watch?v=dQw4w9WgXcQ)\n```\nfunction() {\n return true;\n}\n```\n> Blockquote\n![image](https://miro.medium.com/max/300/0*9U_PkckAUtKGrb_R.png)\n**Bold text**\n- list item.\n`code block`'
     };
     this.handleChange = this.handleChange.bind(this);
   }
   /*As you type, the input state is automatically updated.*/
   handleChange(event) {
+      marked.setOptions({
+        breaks: true
+      });
+
       this.setState({
         input: event.target.value
       });
-    }
+  }
 
   render() {
     const {input} = this.state;
@@ -24,11 +31,12 @@ class Code extends React.Component {
     return (
       <div>
         <div className="editor-div">
+          <h1 class="previewer-heading">Your code</h1>
           <textarea id="editor" value={this.state.input} onChange={this.handleChange} rows="10" cols="80"></textarea>
         </div>
 
         <div id="preview-div">
-          <h1>Result</h1>
+          <h1 class="previewer-heading" id="result">Result</h1>
           <div id="preview" dangerouslySetInnerHTML={{__html: markdown}}>
           </div>
         </div>
